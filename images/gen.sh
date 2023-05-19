fa2f7686ee0d1585cb5f5baee3c5ba96b0293d90
Titus Ong <titusongyl@gmail.com>
2023-05-19 19:08:31 +0800

    feat: add keymap graphic generator

---
+++ images/gen.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# See https://github.com/caksoylar/keymap-drawer for more info.
+
+# Requires python 3.10
+script_dir=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+parent_dir=$(builtin cd "$script_dir/.."; pwd)
+keymap parse -c 10 -z "$parent_dir/config/sofle.keymap" > "$script_dir/sofle_keymap.yaml"
+keymap draw "$script_dir/sofle_keymap.yaml" > "$script_dir/sofle_keymap.ortho.svg"
