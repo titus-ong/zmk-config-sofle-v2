8d25cd19d56e03004591b0f4cb9d950592ecbae1
Titus Ong <titusongyl@gmail.com>
2023-05-22 16:17:07 +0800

    feat: add back parsed keymap for tracking

--- images/gen.sh
+++ images/gen.sh
@@ -6,4 +6,3 @@ script_dir=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 parent_dir=$(builtin cd "$script_dir/.."; pwd)
 keymap -c "$script_dir/config.yaml" parse -c 14 -z "$parent_dir/config/sofle.keymap" > "$script_dir/sofle.yaml"
 keymap -c "$script_dir/config.yaml" draw "$script_dir/sofle.yaml" > "$script_dir/sofle.svg"
-rm "$script_dir/sofle.yaml"
